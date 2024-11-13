e0a1906a93316309ae9e830305186a36e62068fa
smarsimo <simonuccimarco#gmail.com>
2024-11-13 16:22:20 +0100

    modified files

--- esercitazione 5/primo_eserc_es_5.py
+++ esercitazione 5/primo_eserc_es_5.py
@@ -5,8 +5,19 @@ sys.path.append('~/Scrivania/metodi	computazionali/esercizi-corso-metodi/somme.p
 import somme 
 
 n=input('inserisci un numero n: ')
+esp=input('inserisci un esponente: ')
+exp=int(esp)
 nn=int(n)
+"""
 print('somma dei primi', nn, 'numeri:', somme.somma(nn))
 print('somma delle radici dei primi', nn, 'numeri:', somme.somma_radici(nn))
+"""
+#chiamo anche le funzioni introdotte dall'esercizio 1 della quinta
+#esercitazione
 
+s=somme.som_prod(nn)
+print('somma dei primi', nn, 'numeri:', s[0])
+print('prodotto dei primi', nn, 'numeri:', s[1])
+Sum=somme.sommatoria(nn,alpha=exp)
+print('la somma dei primi', nn, 'numeri elevati alla', exp, 'è:', Sum)
 
--- esercitazione 5/somme.py
+++ esercitazione 5/somme.py
@@ -1,9 +1,8 @@
 import numpy as np
-
+"""
 def somma(*args):
-	"""
-	Funzione che restituisce la somma dei primi n numeri naturali
-	"""
+
+	#Funzione che restituisce la somma dei primi n numeri naturali
 	
 	sum=0
 	for i in range(args[0]+1):
@@ -14,12 +13,50 @@ def somma(*args):
 #n numeri naturali con n da passare tramite un argomento
 
 def somma_radici(*args):
-	"""
-	Funzione che restituisce la somma delle radici dei primi n numeri 
-	naturali 
-	"""
+	
+	#Funzione che restituisce la somma delle radici dei primi n numeri 
+	#naturali 
+
 	sum=0
 	for i in range(args[0]+1):
 		sum=sum+np.sqrt(i)
 	return sum
+
+"""
+
+def som_prod(*args):
+	
+	#funzione che restituisce somma e prodotto dei primi n numeri naturali
+	
+	sum=0
+	prod=1
+	for i in range(args[0]):
+		sum=sum+(i+1)
+		prod=prod*(i+1)
+	return sum, prod
+	
+
+def sommatoria(*args, **kwargs):
+	#funzione che restituisce la somma per i che va da 0 a n
+	#di i^alpha
+	#--------------
+	#PARAMETRI:
+	#n: numero al quale si ferma la sommatoria
+	#alpha: esponente con valore di default uguale a 1
+	alpha=1
+	sommatoria=0
+	for i in range(args[0]+1):
+		sommatoria=sommatoria+pow(i,alpha)
+	return sommatoria
+
+	
+	
+
+
+
+
+
+
+
+
 
